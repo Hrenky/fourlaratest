215f4ab9d0289338ccff4d729d02b024083f3467
Ivan Hrenovac <ivan.hrenovac@gmail.com>
2023-10-11 02:41:55 +0200

    Project finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
     <head>
         @include('parts.meta')
         @include('assets.master-style')
+        @stack('styles')
     </head>
     <body class="bg-white d-flex flex-column min-vh-100">
-        @stack('styles')
 
         <div id="head" class="container-fluid">
             <div class="row bg-primary p-3">
